baec86d5bfa2c155a2918d37a5cca5f0ead3c8d7
Thomson K (Kirsty) <Kirsty.Thomson@gov.scot>
2025-11-13 17:12:16 +0000

    Start of confidence interval summaries

--- 7. CPDS-November-Production-Confidence Intervals.R
+++ 7. CPDS-November-Production-Confidence Intervals.R
@@ -1,2 +1,32 @@
 ### Confidence intervals ###
-Confidence_intervals 
+# Define target crops
+target_crops <- c("Total_cereals", "Barley S", "Barley W", "Wheat", "Oats", "OSRape")
+
+# Filter and summarize
+summary_table <- production_raised_ITL225_2 %>%
+  filter(Crop %in% target_crops,  Region == "Scotland") %>%
+  left_join(descriptive_stats_combined, by = c("Crop", "Region")) %>%
+  mutate(
+    se_yield = sd_yield / sqrt(returns_descriptive),
+    se_production = se_yield * census_area,
+    ci_limit = 1.96 * se_production,
+    ci_percent = (1.96 * se_production / production_raised) * 100,
+    sampling_percent = (`Number of returns` / `Number of holdings`) * 100
+  ) %>%
+  select(
+    Crop,
+    `Number of holdings`,
+    `Number of returns`,
+    sampling_percent,
+    production_raised,
+    ci_limit,
+    ci_percent
+  ) %>%
+  rename(
+    `Sampling %` = sampling_percent,
+    `Production raised` = production_raised,
+    `Confidence limit` = ci_limit,
+    `Confidence %` = ci_percent
+  )
+
+
